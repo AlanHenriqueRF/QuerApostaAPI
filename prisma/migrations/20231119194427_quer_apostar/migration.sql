522e0622ce10f15f33591fb4ccafca8590f89de5
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-11-19 17:17:00 -0300

    fix: Configuring and adjusting the project environment

---
+++ prisma/migrations/20231119194427_quer_apostar/migration.sql
@@ -0,0 +1,55 @@
+-- CreateEnum
+CREATE TYPE "BetStatus" AS ENUM ('PENDING', 'WON', 'LOST');
+
+-- CreateTable
+CREATE TABLE "Participant" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "balance" INTEGER NOT NULL,
+
+    CONSTRAINT "Participant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "homeTeamName" TEXT NOT NULL,
+    "awayTeamName" TEXT NOT NULL,
+    "homeTeamScore" INTEGER NOT NULL,
+    "awayTeamScore" INTEGER NOT NULL,
+    "isFinished" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Bet" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "homeTeamScore" INTEGER NOT NULL,
+    "awayTeamScore" INTEGER NOT NULL,
+    "amountBet" INTEGER NOT NULL,
+    "gameId" INTEGER NOT NULL,
+    "participantId" INTEGER NOT NULL,
+    "status" "BetStatus" NOT NULL,
+    "amountWon" INTEGER NOT NULL,
+
+    CONSTRAINT "Bet_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Bet_gameId_key" ON "Bet"("gameId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Bet_participantId_key" ON "Bet"("participantId");
+
+-- AddForeignKey
+ALTER TABLE "Bet" ADD CONSTRAINT "Bet_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bet" ADD CONSTRAINT "Bet_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "Participant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
-import express, { Request, Response, json } from 'express';
+import express, { Request, Response, json, Express } from 'express';
 import cors from 'cors';
+import { loadEnv, connectDb, disconnectDB, prisma } from '@/config';
+
+loadEnv();
 
 const app = express();
-app.use(cors())
-app.use(json());
+app
+    .use(cors())
+    .use(json())
+    .get('/health', async (req: Request, res: Response) => {res.status(200).send('ok ta funcionando')})
 
-app.get('/health', (req: Request, res: Response) => {
-  res.status(200).send("I'm ok!");
-});
+
+export function init(): Promise<Express> {
+    connectDb();
+    return Promise.resolve(app);
+}
+
+export async function close(): Promise<void> {
+    await disconnectDB();
+}
 
 export default app;
